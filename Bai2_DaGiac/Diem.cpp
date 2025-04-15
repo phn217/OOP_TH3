e09b30499dc947feed9af54b38f81f151c76aada
phn217 <phinhung21012006@gmail.com>
2025-04-15 21:24:34 +0700

    Diem.cpp

--- Bai2_DaGiac/Diem.cpp
+++ Bai2_DaGiac/Diem.cpp
@@ -5,15 +5,12 @@ Diem::Diem()
 	dx = 0;
 	dy = 0;
 }
-
 Diem::Diem(double x, double y)
 {
 	dx = x;
 	dy = y;
 }
-
 Diem::~Diem() {}
-
 void Diem::Nhap()
 {
 	cout << "Nhap hoanh do: ";
@@ -21,27 +18,22 @@ void Diem::Nhap()
 	cout << "Nhap tung do: ";
 	cin >> dy;
 }
-
 void Diem::Xuat()
 {
 	cout << "(" << dx << "," << dy << ")\n";
 }
-
 double Diem::GetX()
 {
 	return dx;
 }
-
 double Diem::GetY()
 {
 	return dy;
 }
-
 double Diem::TinhKhoangCach(Diem& d)
 {
 	return sqrt(pow(dx - d.dx, 2) + pow(dy - d.dy, 2));
 }
-
 void Diem::TinhTien(double x, double y)
 {
 	dx += x;
